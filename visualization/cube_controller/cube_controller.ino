e5668361808a3c709d5bd46197fffbb1eeb57675
Kelvin <ma.kel@husky.neu.edu>
2020-02-22 16:56:48 -0500

    Python to arduino test code

--- visualization/cube_controller/cube_controller.ino
+++ visualization/cube_controller/cube_controller.ino
@@ -4,11 +4,17 @@
 int redValue = 0;
 int blueValue = 0;
 int greenValue = 0;
+int clearColors [][3] = {
+  {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
+  {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
+  {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
+  {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}
+};
 int testArrayofColors [][3] = {
-  {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}
+  {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}
 };
 
-const byte buffSize = 128;
+const byte buffSize = 255;
 char inputBuffer[buffSize];
 const char startMarker = '<';
 const char endMarker = '>';
@@ -17,14 +23,8 @@ boolean readInProgress = false;
 boolean newDataFromPC = false;
 
 char messageFromPC[buffSize] = {0};
-int newFlashInterval = 0;
-float servoFraction = 0.0; // fraction of servo range to move
-
 
 unsigned long curMillis;
-
-unsigned long prevReplyToPCmillis = 0;
-unsigned long replyToPCinterval = 1000;
 
 // A basic everyday NeoPixel strip test program.
 
@@ -138,23 +138,12 @@ void setup() {
   Serial.println("<Arduino is ready>");
   
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.show();
+  strip.setBrightness(20); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
 
 // loop() function -- runs repeatedly as long as board is on ---------------
-
-/*
- * 
- * void loop() {
- *    while (Serial.available()){
- *        data = Serial.read();
- *    }
- * }
- */
-
-
 void loop() {
     curMillis = millis();
     getDataFromPC();
@@ -162,30 +151,6 @@ void loop() {
 //    strip.show();
     showColors(testArrayofColors);
     replyToPC();
-
-
-
-//    
-//    showColors(arrayOfColorValues1);
-//    showColors(arrayOfColorValues2);
-//    showColors(arrayOfColorValues3);
-//    showColors(arrayOfColorValues4);
-
-
-//  
-//  // Fill along the length of the strip in various colors...
-//  colorWipe(strip.Color(  255, 0,   0), 50); // Red
-//  colorWipe(strip.Color(  0, 255,   0), 50); // Green
-//  colorWipe(strip.Color(  0,   0, 255), 50); // Blue
-//
-//
-//  // Do a theater marquee effect in various colors...
-//  theaterChase(strip.Color(127, 127, 127), 50); // White, half brightness
-//  theaterChase(strip.Color(127,   0,   0), 50); // Red, half brightness
-//  theaterChase(strip.Color(  0,   0, 127), 50); // Blue, half brightness
-////
-//  rainbow(10);             // Flowing rainbow cycle along the whole strip
-//  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
 }
 
 // Grabs the individual rgb valeus from an uint32_t Color
@@ -225,8 +190,8 @@ void getDataFromPC() {
 
   // receive data from PC and save it into inputBuffer
 
-  if (Serial.available() > 0) {
-
+//  if (Serial.available() > 0 && newDataFromPC == false) {
+  while (Serial.available() > 0 && newDataFromPC == false) {
     char x = Serial.read();
 
     // the order of these IF clauses is significant
@@ -270,7 +235,7 @@ void parseData() {
   strtokIndx = strtok(NULL, "."); // this continues where the previous call left off
   testArrayofColors[0][2] = atoi(strtokIndx);     // convert this part to an integer
 
-  for (int a = 1; a < 4; a++) {
+  for (int a = 1; a < 5; a++) {
     strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
     testArrayofColors[a][0] = atoi(strtokIndx);     // convert this part to an integer
   
@@ -280,16 +245,6 @@ void parseData() {
     strtokIndx = strtok(NULL, "."); // this continues where the previous call left off
     testArrayofColors[a][2] = atoi(strtokIndx);     // convert this part to an integer
   }
-
-  //  strtokIndx = strtok(inputBuffer, ",");     // get the first part - the string
-  //  strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
-  //
-  //  strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-  //  newFlashInterval = atoi(strtokIndx);     // convert this part to an integer
-  //
-  //  strtokIndx = strtok(NULL, ",");
-  //  servoFraction = atof(strtokIndx);     // convert this part to a float
-
 }
 
 //=============
@@ -298,119 +253,45 @@ void replyToPC() {
 
   if (newDataFromPC) {
     newDataFromPC = false;
+
     Serial.print("<Msg ");
     Serial.print(messageFromPC);
     Serial.print(" Red1 ");
-    Serial.print(testArrayofColors[0][0]);
+    Serial.print(testArrayofColors[32][0]);
     Serial.print(" Green1 ");
-    Serial.print(testArrayofColors[0][1]);
+    Serial.print(testArrayofColors[32][1]);
     Serial.print(" Blue1 ");
-    Serial.print(testArrayofColors[0][2]);
-    Serial.print(" Red2 ");
-    Serial.print(testArrayofColors[1][0]);
-    Serial.print(" Green2 ");
-    Serial.print(testArrayofColors[1][1]);
-    Serial.print(" Blue2 ");
-    Serial.print(testArrayofColors[1][2]);
-    Serial.print(" Red3 ");
-    Serial.print(testArrayofColors[2][0]);
-    Serial.print(" Green3 ");
-    Serial.print(testArrayofColors[2][1]);
-    Serial.print(" Blue3 ");
-    Serial.print(testArrayofColors[2][2]);
-    Serial.print(" Red4 ");
-    Serial.print(testArrayofColors[3][0]);
-    Serial.print(" Green4 ");
-    Serial.print(testArrayofColors[3][1]);
-    Serial.print(" Blue4 ");
-    Serial.print(testArrayofColors[3][2]);
-    Serial.print(" Time ");
-    Serial.print(curMillis >> 9); // divide by 512 is approx = half-seconds
+    Serial.print(testArrayofColors[32][2]);
     Serial.println(">");
+
+//    Serial.print("<Msg ");
+//    Serial.print(messageFromPC);
+//    Serial.print(" Red1 ");
+//    Serial.print(testArrayofColors[0][0]);
+//    Serial.print(" Green1 ");
+//    Serial.print(testArrayofColors[0][1]);
+//    Serial.print(" Blue1 ");
+//    Serial.print(testArrayofColors[0][2]);
+//    Serial.print(" Red2 ");
+//    Serial.print(testArrayofColors[1][0]);
+//    Serial.print(" Green2 ");
+//    Serial.print(testArrayofColors[1][1]);
+//    Serial.print(" Blue2 ");
+//    Serial.print(testArrayofColors[1][2]);
+//    Serial.print(" Red3 ");
+//    Serial.print(testArrayofColors[2][0]);
+//    Serial.print(" Green3 ");
+//    Serial.print(testArrayofColors[2][1]);
+//    Serial.print(" Blue3 ");
+//    Serial.print(testArrayofColors[2][2]);
+//    Serial.print(" Red4 ");
+//    Serial.print(testArrayofColors[3][0]);
+//    Serial.print(" Green4 ");
+//    Serial.print(testArrayofColors[3][1]);
+//    Serial.print(" Blue4 ");
+//    Serial.print(testArrayofColors[3][2]);
+//    Serial.print(" Time ");
+//    Serial.print(curMillis >> 9); // divide by 512 is approx = half-seconds
+//    Serial.println(">");
   }
 }
-
-
-
-
-//
-//
-//
-//// Default test functions given with gamma correction (in case we want to see it)
-//// Some functions of our own for creating animated effects -----------------
-//
-//// Fill strip pixels one after another with a color. Strip is NOT cleared
-//// first; anything there will be covered pixel by pixel. Pass in color
-//// (as a single 'packed' 32-bit value, which you can get by calling
-//// strip.Color(red, green, blue) as shown in the loop() function above),
-//// and a delay time (in milliseconds) between pixels.
-//void colorWipe(uint32_t color, int wait) {
-//  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-//    setColorGammaCorrect(i, color);         //  Set pixel's color (in RAM)
-//    strip.show();                          //  Update strip to match
-////    delay(wait);                           //  Pause for a moment
-//  }
-//}
-//
-//// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
-//// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
-//// between frames.
-//void theaterChase(uint32_t color, int wait) {
-//  for(int a=0; a<10; a++) {  // Repeat 10 times...
-//    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-//      strip.clear();         //   Set all pixels in RAM to 0 (off)
-//      // 'c' counts up from 'b' to end of strip in steps of 3...
-//      for(int c=b; c<strip.numPixels(); c += 3) {
-//        setColorGammaCorrect(c, color); // Set pixel 'c' to value 'color'
-//      }
-//      strip.show(); // Update strip with new contents
-//      delay(wait);  // Pause for a moment
-//    }
-//  }
-//}
-//
-//// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-//void rainbow(int wait) {
-//  // Hue of first pixel runs 5 complete loops through the color wheel.
-//  // Color wheel has a range of 65536 but it's OK if we roll over, so
-//  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-//  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
-//  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-//    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-//      // Offset pixel hue by an amount to make one full revolution of the
-//      // color wheel (range of 65536) along the length of the strip
-//      // (strip.numPixels() steps):
-//      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-//      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-//      // optionally add saturation and value (brightness) (each 0 to 255).
-//      // Here we're using just the single-argument hue variant. The result
-//      // is passed through strip.gamma32() to provide 'truer' colors
-//      // before assigning to each pixel:
-//      setColorGammaCorrect(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-//    }
-//    strip.show(); // Update strip with new contents
-//    delay(wait);  // Pause for a moment
-//  }
-//}
-//
-//// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
-//void theaterChaseRainbow(int wait) {
-//  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
-//  for(int a=0; a<30; a++) {  // Repeat 30 times...
-//    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-//      strip.clear();         //   Set all pixels in RAM to 0 (off)
-//      // 'c' counts up from 'b' to end of strip in increments of 3...
-//      for(int c=b; c<strip.numPixels(); c += 3) {
-//        // hue of pixel 'c' is offset by an amount to make one full
-//        // revolution of the color wheel (range 65536) along the length
-//        // of the strip (strip.numPixels() steps):
-//        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
-//        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-//        setColorGammaCorrect(c, color); // Set pixel 'c' to value 'color'
-//      }
-//      strip.show();                // Update strip with new contents
-//      delay(wait);                 // Pause for a moment
-//      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
-//    }
-//  }
-//}
