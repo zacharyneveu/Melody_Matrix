62a405122f4b89a91e903089d3701a03ee26dd7f
Teng Z <35406893+URpineapple@users.noreply.github.com>
2020-03-18 23:30:09 -0400

    Added random frames rendering feature

--- web_app/sphere_demo2.js
+++ web_app/sphere_demo2.js
@@ -1,65 +1,103 @@
-var datas;
-fetch('http://127.0.0.1:5500/data.json').then((response) => {
-    return response.json()
-})
-    .then((data) => {
-        datas = data
-    });
+var color_data = { "0": { "0": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "1": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "2": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "3": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "4": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "5": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"] }, "1": { "0": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "1": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "2": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "3": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "4": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "5": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"] }, "2": { "0": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "1": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "2": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "3": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "4": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "5": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"] }, "3": { "0": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "1": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "2": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "3": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "4": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "5": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"] }, "4": { "0": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "1": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "2": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "3": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "4": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "5": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"] }, "5": { "0": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "1": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "2": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "3": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "4": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"], "5": ["0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"] } }
+
+var urls = "http://127.0.0.1:5500/data.json";
+let fr = 500
+
+// $(document).ready(function loadData() {
+//     //loadJSON(urls, gotData)
+//     $.ajax({
+//         url: "http://127.0.0.1:5500/data.json",
+//         type: 'GET',
+//         dataType: 'json',
+//         success: function (res) {
+//             console.log(res);
+//             color_data = res
+//             setTimeout(loadData, 1000);
+//         }
+//     });
+// })
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
+    loadData()
+    frameRate(fr);
     easycam = createEasyCam();
     document.oncontextmenu = () => false;
-    easycam.setDistance(1500, 0);
+    easycam.setDistance(800, 0);
 }
 
-function draw() {
-    // let maxOffset = min(400, width / 2, height / 2)
+function gotData(data) {
+    storeItem('color_data', data)
+    //console.log('gotdata', color_data)
+}
 
-    function colorPart(x_value, y_value, z_value) {
-        let arr = datas[5 - y_value][5 - z_value][x_value]
-        return arr.split(',')
-    }
+function loadData() {
+    loadJSON(urls, gotData)
+    // $.ajax({
+    //     url: "http://127.0.0.1:5500/data.json",
+    //     type: 'GET',
+    //     dataType: 'json',
+    //     success: function (res) {
+    //         console.log(res);
+    //         color_data = res
+    //         //setTimeout(loadData, 1000);
+    //     }
+    // });
 
-    function forRange(fn) {
-        const cubeSpacing = 100
-        for (let i = -250; i <= 250; i += cubeSpacing) {
-            fn(i);
+}
+
+if (color_data) {
+    function draw() {
+        color_data = getItem('color_data')
+
+        function colorPart(x_value, y_value, z_value) {
+            let arr = color_data[5 - y_value][5 - z_value][x_value]
+            return arr.split(',')
         }
+
+        function forRange(fn) {
+            const cubeSpacing = 100
+            for (let i = -250; i <= 250; i += cubeSpacing) {
+                fn(i);
+            }
+        }
+
+        function coordToIndex(num) {
+            return (num / 50 + 5) / 2
+        }
+
+        background(155);
+        translate(0, 0, -500);
+        //rotateY(PI / 4)
+
+        // rotateX(PI/4)
+        // rotateX(millis() / 2000);
+        // rotateZ(millis() / 2000);
+        rotateY(millis() / 2000);
+
+        // let size = Math.random() % 10 *25
+        // let volume = Math.random() % 1 + 1
+        let volume = 1
+        forRange(x => forRange(y => forRange(z => {
+            let pos = createVector(x, y, z);
+            // const shrinkNeeded = changingMaxRadius.mag() / pos.mag();
+            // if (shrinkNeeded < 1) {
+            //     pos = pos.mult(shrinkNeeded);
+            // }
+            noStroke()
+            push();
+            translate(volume * pos.x, volume * pos.y, volume * pos.z);
+            let index_x = coordToIndex(x)
+            let index_y = coordToIndex(y)
+            let index_z = coordToIndex(z)
+
+
+            let tem_arr = colorPart(index_x, index_y, index_z)
+            fill(parseInt(tem_arr[0]), parseInt(tem_arr[1]), parseInt(tem_arr[2]));
+
+            sphere(18)
+            //console.log(size)
+            pop();
+        })))
     }
-
-    function coordToIndex(num) {
-        return (num / 50 + 5) / 2
-    }
-
-    background(155);
-    // translate(0, 0, -500);
-    // rotateX(PI/4)
-    // rotateX(millis() / 2000);
-    //rotateZ(millis() / 2000);
-    //rotateY(millis() / 1000);
-    // const cosOverTime = cos(millis() / 2000)
-    // const changingMaxRadius = createVector(map(cosOverTime, -1, 1, maxOffset, maxOffset * 2), 0, 0);
-
-    forRange(x => forRange(y => forRange(z => {
-        let pos = createVector(x, y, z);
-        // const shrinkNeeded = changingMaxRadius.mag() / pos.mag();
-        // if (shrinkNeeded < 1) {
-        //     pos = pos.mult(shrinkNeeded);
-        // }
-        noStroke()
-        push();
-        translate(pos.x, pos.y, pos.z);
-        let index_x = coordToIndex(x)
-        let index_y = coordToIndex(y)
-        let index_z = coordToIndex(z)
-        let tem_arr = colorPart(index_x, index_y, index_z)
-        // if(index_y == 0){
-        //     fill(255, 0 ,0)
-        // }
-        fill(parseInt(tem_arr[0]), parseInt(tem_arr[1]), parseInt(tem_arr[2]));
-        sphere(18)
-        pop();
-    })))
-
 }
