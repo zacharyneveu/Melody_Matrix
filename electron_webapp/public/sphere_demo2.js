fcbabf4a021ad2830896e828dc9175d389925711
Connor <rog.c@husky.neu.edu>
2020-03-22 16:44:06 -0400

    erased duplicate easycam creation

--- electron_webapp/public/sphere_demo2.js
+++ electron_webapp/public/sphere_demo2.js
@@ -10,7 +10,6 @@ function setup() {
         n = n || o.renderer,
           n && (this.camEYE = this.getPosition(this.camEYE), this.camLAT = this.getCenter(this.camLAT), this.camRUP = this.getUpVector(this.camRUP), n._curCamera.camera(this.camEYE[0], this.camEYE[1], this.camEYE[2], this.camLAT[0], this.camLAT[1], this.camLAT[2], this.camRUP[0], this.camRUP[1], this.camRUP[2]))
       };
-    easycam = createEasyCam();
     easycam = createEasyCam(this._renderer, {distance:1000});
     document.oncontextmenu = () => false;
 }
