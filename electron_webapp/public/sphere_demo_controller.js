78f696298bd1eed9335c776db2b13a86d8c78cd9
petridecus <apetrides32@gmail.com>
2020-03-27 14:38:51 -0400

    Adding in timer function to controller

--- electron_webapp/public/sphere_demo_controller.js
+++ electron_webapp/public/sphere_demo_controller.js
@@ -3,18 +3,27 @@ let sphereModel = require('./sphere_demo_model.js');
 class CubeController {
     constructor () {
         this.cubeModel = new sphereModel.CubeModel();
+	this.timer = this.createTimer();
     }
 
     dataRequest() {
-        this.cubeModel.updateNotes();
-        // TODO: figure out how to only updateGenre() every n seconds or something
-        this.cubeModel.updateGenre(); 
+	this.cubeModel.updateNotes();
+	this.cubeModel.updateGenre();
         console.log(JSON.stringify(this.cubeModel.modelData));
         return this.cubeModel.modelData;
     }
 
+    updateModel() {
+        this.cubeModel.updateNotes();
+        this.cubeModel.updateGenre();
+    }
+
     handleReceiveData(jsonData) {
         this.cubeModel.updateData(jsonData);
+    }
+
+    createTimer() {
+	return setInterval(this.updateModel, 1000); 
     }
 };
 
